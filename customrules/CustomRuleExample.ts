a81d788801d20d31cb24030ce9321057698e32a6
Ruben <halmanrf@gmail.com>
2024-03-19 19:07:38 +0100

    update import

--- customrules/CustomRuleExample.ts
+++ customrules/CustomRuleExample.ts
@@ -1,6 +1,6 @@
-import { Flow, FlowAttribute, FlowType, IRuleDefinition, ResultDetails, RuleResult } from '/Users/rubenhalman/Projects/lightning-flow-scanner-core/src/index';
+const types = await import('https://Lightning-Flow-Scanner.github.io/lightning-flow-scanner-core/types.d.ts');
 
-export class CustomNamingConvention implements IRuleDefinition{
+export class CustomNamingConvention implements types.IRuleDefinition{
 
   name: string;
   label: string;
@@ -15,15 +15,15 @@ export class CustomNamingConvention implements IRuleDefinition{
     this.label = 'Custom Naming Convention';
     this.description='custom execute function ';
     this.type = 'flow';
-    this.supportedTypes = FlowType.allTypes();
+    this.supportedTypes = types.FlowType.allTypes();
     isConfigurable: true;
   }
 
-  public execute(flow: Flow, options?: { expression: string }): RuleResult {
+  public execute(flow: types.Flow, options?: { expression: string }): types.RuleResult {
 
     const conventionApplied = (flow.name)?.includes('_');
     return (!conventionApplied ?
-      new RuleResult(this, [new ResultDetails(new FlowAttribute(flow.name, 'name', 'The Name needs to include a _'))]) :
-      new RuleResult(this, []));
+      new types.RuleResult(this, [new types.ResultDetails(new types.FlowAttribute(flow.name, 'name', 'The Name needs to include a _'))]) :
+      new types.RuleResult(this, []));
   }
 }
