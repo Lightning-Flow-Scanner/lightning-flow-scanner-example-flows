adc3695841c43bd1ef7b1df5c0a4ffa22229f53f
Ruben <halmanrf@gmail.com>
2024-03-19 18:29:22 +0100

    include config & custom rule example

---
+++ customrules/CustomRuleExample.ts
@@ -0,0 +1,29 @@
+import { Flow, FlowAttribute, FlowType, IRuleDefinition, ResultDetails, RuleResult } from '/Users/rubenhalman/Projects/lightning-flow-scanner-core/src/index';
+
+export class CustomNamingConvention implements IRuleDefinition{
+
+  name: string;
+  label: string;
+  description: string;
+  type:string;
+  supportedTypes:string[];
+  isConfigurable: boolean;
+  docRefs: any;
+
+  constructor(){
+    this.name = 'CustomNamingConvention';
+    this.label = 'Custom Naming Convention';
+    this.description='custom execute function ';
+    this.type = 'flow';
+    this.supportedTypes = FlowType.allTypes();
+    isConfigurable: true;
+  }
+
+  public execute(flow: Flow, options?: { expression: string }): RuleResult {
+
+    const conventionApplied = (flow.name)?.includes('_');
+    return (!conventionApplied ?
+      new RuleResult(this, [new ResultDetails(new FlowAttribute(flow.name, 'name', 'The Name needs to include a _'))]) :
+      new RuleResult(this, []));
+  }
+}
